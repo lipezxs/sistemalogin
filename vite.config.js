23bd0bb39b9ad9138c948e209e3d8e09161b08d9
lipezxs <fa8561141@gmail.com>
2025-03-15 19:22:36 -0300

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: '0.0.0.0', // Escuta em todas as interfaces de rede
+    port: process.env.PORT || 5173, // Usa a porta do Render ou 5173 como fallback
+  },
+};
